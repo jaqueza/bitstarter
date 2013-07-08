9ee6f930688c1cc5b8e2585de94d4956594fa6c2
Rommel Marcelo <rommel.marcelo@gmail.com>
2013-07-08 01:26:27 +0000

    Modified web.js to read from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var tmp = fs.readFileSync('index.html');
+var buf = new Buffer(tmp, 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('utf8',0, tmp.length));
 });
 
 var port = process.env.PORT || 5000;
