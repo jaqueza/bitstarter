de4bf372504a66f5d469f0411bb28d997aac2e02
mhel <jaqueza@gmail.com>
2013-07-17 18:00:49 -0400

    Copied a more elegant version of web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html"
+
 var app = express.createServer(express.logger());
-var tmp = fs.readFileSync('index.html');
-var buf = new Buffer(tmp, 'utf8');
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8',0, tmp.length));
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
